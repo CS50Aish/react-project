0a5f53a64b01b6d97b62ad8fa82d3e3cf9faf8d8
Ubuntu <labuser@ubuntu2204.3gep0rv0rjguliu2mwl1vyf3ca.rx.internal.cloudapp.net>
2024-07-07 13:12:44 +0000

    React JS Project Files

---
+++ react/myapp/src/Bike.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Bike extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            make:"Yamaha",
+            model:"R15",
+            color:"blue"
+        }
+    };
+
+    changeBikeColor
+}
