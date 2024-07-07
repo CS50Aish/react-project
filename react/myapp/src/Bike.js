09a9aca22b1b4c08c21fba2938743d76a9f73e65
Ubuntu <labuser@ubuntu2204.3gep0rv0rjguliu2mwl1vyf3ca.rx.internal.cloudapp.net>
2024-07-07 13:23:11 +0000

    React JS Project Files

--- react/myapp/src/Bike.js
+++ react/myapp/src/Bike.js
@@ -10,5 +10,19 @@ class Bike extends React.Component{
         }
     };
 
-    changeBikeColor
+    changeBikeColor = () => {
+        this.setState({color:"black"});
+    }
+
+    render(){
+        return (
+            <div>
+                <h2>My {this.state.make}</h2>
+                <p>It is a {this.state.color} {this.state.model} </p>
+                <button type="button" onClick={this.changeBikeColor}>Change Color</button>
+            </div>
+        )
+    }
 }
+
+export default Bike;
