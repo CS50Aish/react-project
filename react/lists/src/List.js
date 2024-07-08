8c953f2e875c0acb5410f69c5d537f04430d732a
Ubuntu <labuser@ubuntu2204.ejxyfxlpt0luflh50m23wpk0xb.rx.internal.cloudapp.net>
2024-07-08 11:38:08 +0000

    Added Files

--- react/lists/src/List.js
+++ react/lists/src/List.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function bookList({books}){
+function BookList({books}){
     return (
         <ul>
             {books.map(book => (
@@ -14,16 +14,19 @@ function bookList({books}){
 }
 
 export default function List(){
-    const books = [
-        {id:1, title:'Book1', author:'Author1'},
-        {id:2, title:'Book2', author:'Author2'},
-        {id:3, title:'Book3', author:'Author3'}
+    const books=[
+        {id: 1, title: 'Book1', author: 'Author1'},
+        {id: 2, title: 'Book2', author: 'Author2'},
+        {id: 3, title: 'Book3', author: 'Author3'},
+        {id: 4, title: 'Book4', author: 'Author4'},
+        {id: 5, title: 'Book5', author: 'Author5'},
+        {id: 6, title: 'Book6', author: 'Author6'}
     ];
 
     return (
         <div>
             <h2>Books List</h2>
-            <bookList books={books}></bookList>
+            <BookList books={books}></BookList>
         </div>
-    )
+    );
 }
--- react/lists/src/index.js
+++ react/lists/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import List from './List';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <List items={items}/>
+    <List />
   </React.StrictMode>
 );
 
